0e6dbdc7c4a4ea247000f8e4bbe4ec7f2b778464
BooraKaushik <87960952+BooraKaushik@users.noreply.github.com>
2022-02-28 14:34:57 -0500

    a5 JavaScript sp22

--- public/labs/a5/js/todos/index.js
+++ public/labs/a5/js/todos/index.js
@@ -1,5 +1,5 @@
 import TodoList from "./TodoList.js";
-// alert("Hello World!");
+alert("Hello World!");
 $('#wd-todo').append(`
     <div class="container">
         <h1>Todo example</h1>
--- public/tuiter/PostItems/index.js
+++ public/tuiter/PostItems/index.js
@@ -1,5 +1,5 @@
 import Posts from "./Posts.js";
-import PostList from "./PostList.js";
+import PostList from "./BookmarkItems.js";
 const book = () => {
     return `<div>${Posts.map((element)=>{return PostList(element)}).join("")}</div>`;
 
