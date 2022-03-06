0f0dfa969f35375211f669351d6f12170912f96e
BooraKaushik <87960952+BooraKaushik@users.noreply.github.com>
2022-03-06 10:08:05 -0500

    a5 JavaScript sp22

--- public/tuiter/WhoToFollowList/index.js
+++ public/tuiter/WhoToFollowList/index.js
@@ -3,7 +3,7 @@ import who from "./who.js";
 const WhoToFollowList = () => {
     return (`<div class="list-group">
     <div class="list-group-item  fa-in"><strong>Who to follow</strong></div>
-            ${who.map((element)=>{return WhoToFollowListItem(element);})}
+            ${who.map((element)=>{return WhoToFollowListItem(element)}).join("")}
             </div>
 `);
 }
