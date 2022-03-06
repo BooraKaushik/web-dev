523e3d509b5a4bbf9d7203ea3a8ff2344976ffb5
BooraKaushik <87960952+BooraKaushik@users.noreply.github.com>
2022-03-06 10:03:47 -0500

    a5 JavaScript sp22

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -7,7 +7,7 @@ import ExploreComponent from "./ExploreComponent.js";
     $('#wd-explore').append(`
         <div class="row mt-2">
         <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-            ${NavigationSidebar()}
+            ${NavigationSidebar('explore')}
         </div>
         <div class="col-10 col-lg-6 pe-0">
         ${ExploreComponent()}</div>
--- public/tuiter/HomeScreen/ExploreScreen.js
+++ public/tuiter/HomeScreen/ExploreScreen.js
@@ -6,7 +6,7 @@ import PostSummaryList from "../PostSummaryList/index.js";
     $('#wd-explore').append(`
         <div class="row mt-2">
         <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-            ${NavigationSidebar()}
+            ${NavigationSidebar('home')}
         </div>
         <div class="col-10 col-lg-6 ps-3 pe-0">
         ${book()}</div>
